00b9040557fbff366aaa266afb5cbffb4d34f9b4
Francisco <subercazefran@gmail.com>
2022-11-30 00:58:48 -0300

    act

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ let productos = [
   {
     id: 1,
     nombre: "Rollo de canela",
-    cantidad: 10,
+    cantidad: 1,
     img: "./images/canela.jpg",
     desc: "El anhelado rollo de canela en tus manos.",
     precio: "$ 245",
@@ -10,7 +10,7 @@ let productos = [
   {
     id: 2,
     nombre: "dona rellena",
-    cantidad: 10,
+    cantidad: 1,
     img: "./images/rellena.jpg",
     desc: "Dona rellena de exquisito  chocolate.",
     precio: "$ 232",
@@ -18,7 +18,7 @@ let productos = [
   {
     id: 3,
     nombre: "Dona de frambuesa",
-    cantidad: 10,
+    cantidad: 1,
     img: "./images/frambuesa.jpg",
     desc: "El anhelado rollo de canela en tus manos.",
     precio: "$ 235",
@@ -26,7 +26,7 @@ let productos = [
   {
     id: 4,
     nombre: "Dona de oreo",
-    cantidad: 10,
+    cantidad: 1,
     img: "./images/oreo.jpg",
     desc: "La media docena mas codiciada de nuestra franquicia.",
     precio: "$ 232",
@@ -34,7 +34,7 @@ let productos = [
   {
     id: 5,
     nombre: "Batidos",
-    cantidad: 10,
+    cantidad: 1,
     img: "./images/milkshake.jpg",
     desc: " Podes llevar todas las que queres surtidas! Cambia el precio segun region.",
     precio: "$ 330",
@@ -42,7 +42,7 @@ let productos = [
   {
     id: 6,
     nombre: "Media docena",
-    cantidad: 10,
+    cantidad: 1,
     img: "./images/media.jpg",
     desc: "La mejor frambuesa de la zona, desde el campo a la mesa.",
     precio: "$ 340",
@@ -50,8 +50,25 @@ let productos = [
 ];
 
 let body = "";
+const contenedorCarrito = document.getElementById("carrito-contenedor");
+
+const botonVaciar = document.getElementById("vaciar-carrito");
+
+const precioTotal = document.getElementById("precioTotal");
 
 let carrito = [];
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("carrito")) {
+    carrito = JSON.parse(localStorage.getItem("carrito"));
+    actualizarCarrito();
+  }
+});
+
+botonVaciar.addEventListener("click", () => {
+  carrito.length = 0;
+  actualizarCarrito();
+});
 
 productos.forEach((el) => {
   body += `
@@ -73,19 +90,29 @@ document.getElementById("contenedor").innerHTML = body;
 const agregarAlCarrito = (prodId) => {
   const item = productos.find((producto) => producto.id === prodId);
   carrito.push(item);
+  actualizarCarrito();
   console.log(carrito);
 };
 
+const eliminarDelCarrito = (prodId) => {
+  const item = carrito.find((prod) => prod.id === prodId);
+  const indice = carrito.indexOf(item);
+  carrito.splice(indice, 1);
+  actualizarCarrito();
+};
 const actualizarCarrito = () => {
+  contenedorCarrito.innerHTML = "";
   carrito.forEach((prod) => {
     const div = document.createElement("div");
     div.className = "productoEnCarrito";
-    div.innerHTML`
+    div.innerHTML = `
     <p>${prod.nombre}</p>
     <p>Precio: ${prod.precio}</p>
     <p>Cantidad: <span id="cantidad"> ${prod.cantidad} </span></p>
     <button onclick = "eliminarDelCarrito(${prod.id})" class="boton-eliminar"></button>
     `;
-    con;
+    contenedorCarrito.appendChild(div);
+    localStorage.setItem("carrito", JSON.stringify(carrito));
   });
+  precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.cantidad, 0);
 };
